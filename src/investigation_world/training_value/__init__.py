1d01240ecdda49fb76d78b57a9ec95398e4c11cd
FPC-effortless <eweeewe917@gmail.com>
2026-08-26 04:17:53 +0100

    feat: add training-value experiment package

---
+++ src/investigation_world/training_value/__init__.py
@@ -0,0 +1,11 @@
+from investigation_world.training_value.diagnostic_sft import (
+    build_diagnostic_examples,
+    build_heldout_diagnostic_episodes,
+    score_diagnostic_generator,
+)
+
+__all__ = [
+    "build_diagnostic_examples",
+    "build_heldout_diagnostic_episodes",
+    "score_diagnostic_generator",
+]
