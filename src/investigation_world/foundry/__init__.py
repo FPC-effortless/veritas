e80721d3c962d61e08aafcae34e5fc5232d9bad9
FPC-effortless <eweeewe917@gmail.com>
2026-08-26 15:25:12 +0100

    feat: export restored capability foundry primitives

--- src/investigation_world/foundry/__init__.py
+++ src/investigation_world/foundry/__init__.py
@@ -1,3 +1,9 @@
+from investigation_world.foundry.capability_families import (
+    CapabilityFamily,
+    CapabilityFamilyId,
+    external_investigation_capability_contract,
+    external_investigation_family,
+)
 from investigation_world.foundry.challenges import ChallengeSpec, FailureClass, challenge_from_trace, classify_failure
 from investigation_world.foundry.companyworld import (
     adapt_companyworld_tasks,
@@ -8,6 +14,17 @@ from investigation_world.foundry.companyworld import (
 from investigation_world.foundry.curriculum import TaskPerformance, frontier_priority, select_frontier_tasks
 from investigation_world.foundry.cycle import FoundryCycleConfig, FoundryCycleResult, aggregate_task_performance, run_foundry_cycle
 from investigation_world.foundry.distributions import DistributionPartition, FoundryDistributionManifest, manifest_from_tasks
+from investigation_world.foundry.expert_trajectories import (
+    DemonstrationSet,
+    ExpertiseAssessment,
+    ExpertTrajectory,
+    PreferencePair,
+    TrainingUse,
+    TrajectoryRole,
+    assess_trace,
+    make_preference_pair,
+    qualify_expert_trace,
+)
 from investigation_world.foundry.materializer import (
     FoundryCompanyWorldRuntime,
     FoundryRuntimeConfig,
@@ -45,6 +62,25 @@ from investigation_world.foundry.task_distribution import (
 )
 from investigation_world.foundry.trace_store import append_trace, load_traces, trace_cost
 from investigation_world.foundry.tracing import TracingRuntimeProxy, execute_counterfactual, replay_trace_prefix
+from investigation_world.foundry.training_product import (
+    TrainerKind,
+    TrainingBundle,
+    TrainingExample,
+    TrainingRecipe,
+    compile_training_bundle,
+)
+from investigation_world.foundry.world_calibration import (
+    CalibrationReport,
+    CalibrationSource,
+    CalibrationSourceKind,
+    DependencyTarget,
+    DistributionTarget,
+    ProcedurePrior,
+    WorldCalibrationSpec,
+    calibration_fingerprint,
+    validate_calibration_report,
+    world_manifest_id,
+)
 from investigation_world.foundry.worlds import (
     CompanyWorldBuildPlan,
     CompanyWorldBuildSpec,
@@ -55,20 +91,24 @@ from investigation_world.foundry.worlds import (
 )
 
 __all__ = [
-    "CapabilityBundle", "CapabilityContract", "ChallengeSpec", "ChallengeValidation", "CompanyWorldBuildPlan",
-    "CompanyWorldBuildSpec", "CounterfactualBranch", "DifficultyDistribution", "DifficultyVector",
-    "DistributionPartition", "DistributionSplit", "EfficiencyPoint", "FailureClass", "FloatRange",
-    "FoundryCompanyWorldRuntime", "FoundryCycleConfig", "FoundryCycleResult", "FoundryDistributionManifest",
-    "FoundryMetrics", "FoundryRuntimeConfig", "FoundryTaskMetadata", "FoundryToolFailure", "GatedRewardContract",
-    "IntRange", "MaterializedCompanyWorldTask", "MutationKind", "MutationLineage", "PromotionPolicy",
-    "RewardWeights", "RolloutTrace", "SampledTaskParameters", "StateSnapshot", "TaskDistributionSpec",
-    "TaskPerformance", "TraceEvent", "TracingRuntimeProxy", "adapt_companyworld_tasks",
-    "aggregate_task_performance", "append_trace", "apply_mutation", "branch_from_snapshot",
-    "challenge_from_trace", "classify_failure", "companyworld_capability_contract", "companyworld_task_metadata",
-    "default_companyworld_build_plan", "execute_counterfactual", "foundry_objective", "frontier_priority",
-    "gated_reward", "infer_companyworld_difficulty", "load_traces", "make_snapshot", "manifest_from_tasks",
-    "materialize_companyworld_build_plan", "materialize_companyworld_task", "pareto_frontier",
-    "patched_generator_source", "promotable", "promotion_failures", "replay_trace_prefix",
-    "run_foundry_cycle", "sample_task_batch", "sample_task_parameters", "select_frontier_tasks",
-    "stable_hash", "trace_cost", "write_companyworld_world_manifest",
+    "CalibrationReport", "CalibrationSource", "CalibrationSourceKind", "CapabilityBundle", "CapabilityContract",
+    "CapabilityFamily", "CapabilityFamilyId", "ChallengeSpec", "ChallengeValidation", "CompanyWorldBuildPlan",
+    "CompanyWorldBuildSpec", "CounterfactualBranch", "DemonstrationSet", "DependencyTarget", "DifficultyDistribution",
+    "DifficultyVector", "DistributionPartition", "DistributionSplit", "DistributionTarget", "EfficiencyPoint",
+    "ExpertTrajectory", "ExpertiseAssessment", "FailureClass", "FloatRange", "FoundryCompanyWorldRuntime",
+    "FoundryCycleConfig", "FoundryCycleResult", "FoundryDistributionManifest", "FoundryMetrics", "FoundryRuntimeConfig",
+    "FoundryTaskMetadata", "FoundryToolFailure", "GatedRewardContract", "IntRange", "MaterializedCompanyWorldTask",
+    "MutationKind", "MutationLineage", "PreferencePair", "ProcedurePrior", "PromotionPolicy", "RewardWeights",
+    "RolloutTrace", "SampledTaskParameters", "StateSnapshot", "TaskDistributionSpec", "TaskPerformance", "TraceEvent",
+    "TracingRuntimeProxy", "TrainerKind", "TrainingBundle", "TrainingExample", "TrainingRecipe", "TrainingUse",
+    "TrajectoryRole", "WorldCalibrationSpec", "adapt_companyworld_tasks", "aggregate_task_performance", "append_trace",
+    "apply_mutation", "assess_trace", "branch_from_snapshot", "calibration_fingerprint", "challenge_from_trace",
+    "classify_failure", "companyworld_capability_contract", "companyworld_task_metadata", "compile_training_bundle",
+    "default_companyworld_build_plan", "execute_counterfactual", "external_investigation_capability_contract",
+    "external_investigation_family", "foundry_objective", "frontier_priority", "gated_reward",
+    "infer_companyworld_difficulty", "load_traces", "make_preference_pair", "make_snapshot", "manifest_from_tasks",
+    "materialize_companyworld_build_plan", "materialize_companyworld_task", "pareto_frontier", "patched_generator_source",
+    "promotable", "promotion_failures", "qualify_expert_trace", "replay_trace_prefix", "run_foundry_cycle",
+    "sample_task_batch", "sample_task_parameters", "select_frontier_tasks", "stable_hash", "trace_cost",
+    "validate_calibration_report", "world_manifest_id", "write_companyworld_world_manifest",
 ]
