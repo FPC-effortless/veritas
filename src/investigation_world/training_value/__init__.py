d1bcbaf601a76df3523dd6b2e2bd801a25412017
FPC-effortless <eweeewe917@gmail.com>
2026-08-26 05:39:51 +0100

    feat: expose verifier-ranked preference data

--- src/investigation_world/training_value/__init__.py
+++ src/investigation_world/training_value/__init__.py
@@ -3,9 +3,11 @@ from investigation_world.training_value.diagnostic_sft import (
     build_heldout_diagnostic_episodes,
     score_diagnostic_generator,
 )
+from investigation_world.training_value.preference import build_verifier_ranked_preferences
 
 __all__ = [
     "build_diagnostic_examples",
     "build_heldout_diagnostic_episodes",
+    "build_verifier_ranked_preferences",
     "score_diagnostic_generator",
 ]
