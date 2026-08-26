cf87dc6273fde04e4d3737462c261086f1b684cd
FPC-effortless <eweeewe917@gmail.com>
2026-08-26 19:24:03 +0100

    Expose Veritas 0.8.0 package version

--- src/investigation_world/__init__.py
+++ src/investigation_world/__init__.py
@@ -3,4 +3,4 @@ from .operational import WorldDomain
 from .veritas import Veritas, VeritasCapability, VeritasCompany, VeritasProductInfo
 from .world.generator import WorldFactory, WorldGenerationConfig, validate_world
 
-__version__ = "0.7.0"
+__version__ = "0.8.0"
