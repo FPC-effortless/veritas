8e7304bac26dc53f91e9527eb9ccd8e0b40d7de4
FPC-effortless <eweeewe917@gmail.com>
2026-08-26 18:27:22 +0100

    Expose unified Veritas capability catalog

--- src/investigation_world/__init__.py
+++ src/investigation_world/__init__.py
@@ -1,6 +1,6 @@
 from .core.models import *
 from .operational import WorldDomain
-from .veritas import Veritas, VeritasCompany, VeritasProductInfo
+from .veritas import Veritas, VeritasCapability, VeritasCompany, VeritasProductInfo
 from .world.generator import WorldFactory, WorldGenerationConfig, validate_world
 
 __version__ = "0.6.0"
