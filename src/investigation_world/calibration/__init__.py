36b203d6af6f8d622028befbae2362bd27376fb1
FPC-effortless <eweeewe917@gmail.com>
2026-08-26 17:03:38 +0100

    feat: export baseline-adjusted calibration helpers

--- src/investigation_world/calibration/__init__.py
+++ src/investigation_world/calibration/__init__.py
@@ -1,7 +1,17 @@
+from investigation_world.calibration.analysis import (
+    attach_baseline_adjusted_scores,
+    baseline_adjusted_model_scores,
+)
 from investigation_world.calibration.full_context import (
     empty_anchors,
     reference_anchors,
     run_full_context_calibration,
 )
 
-__all__ = ["empty_anchors", "reference_anchors", "run_full_context_calibration"]
+__all__ = [
+    "attach_baseline_adjusted_scores",
+    "baseline_adjusted_model_scores",
+    "empty_anchors",
+    "reference_anchors",
+    "run_full_context_calibration",
+]
