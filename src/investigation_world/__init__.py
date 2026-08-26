0b0b63cbb09fc197360ca3087f3d4924a194efd7
FPC-effortless <eweeewe917@gmail.com>
2026-08-26 18:11:39 +0100

    Preserve package wildcard compatibility

--- src/investigation_world/__init__.py
+++ src/investigation_world/__init__.py
@@ -4,12 +4,3 @@ from .veritas import Veritas, VeritasProductInfo
 from .world.generator import WorldFactory, WorldGenerationConfig, validate_world
 
 __version__ = "0.6.0"
-
-__all__ = [
-    "Veritas",
-    "VeritasProductInfo",
-    "WorldDomain",
-    "WorldFactory",
-    "WorldGenerationConfig",
-    "validate_world",
-]
