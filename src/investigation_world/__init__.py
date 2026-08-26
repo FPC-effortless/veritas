da853aa51b3fb99d2264fa24441924aa56ba068f
FPC-effortless <eweeewe917@gmail.com>
2026-08-26 18:15:12 +0100

    Expose persistent Veritas company facade

--- src/investigation_world/__init__.py
+++ src/investigation_world/__init__.py
@@ -1,6 +1,6 @@
 from .core.models import *
 from .operational import WorldDomain
-from .veritas import Veritas, VeritasProductInfo
+from .veritas import Veritas, VeritasCompany, VeritasProductInfo
 from .world.generator import WorldFactory, WorldGenerationConfig, validate_world
 
 __version__ = "0.6.0"
