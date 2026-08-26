571cd7c8cf0a4794b6597e055f27e5cc638683aa
FPC-effortless <eweeewe917@gmail.com>
2026-08-26 23:30:01 +0100

    feat: export SRE commercial evaluation API

--- src/investigation_world/commercial/__init__.py
+++ src/investigation_world/commercial/__init__.py
@@ -1,8 +1,16 @@
 from investigation_world.commercial.manifest import EvaluationManifest
 from investigation_world.commercial.report import build_customer_report, normalize_capability_score
+from investigation_world.commercial.sre_evaluation import (
+    build_sre_prompt,
+    evaluate_sre_generator,
+    parse_sre_prediction,
+)
 
 __all__ = [
     "EvaluationManifest",
     "build_customer_report",
+    "build_sre_prompt",
+    "evaluate_sre_generator",
     "normalize_capability_score",
+    "parse_sre_prediction",
 ]
