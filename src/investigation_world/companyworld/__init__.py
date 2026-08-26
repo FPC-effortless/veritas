7d1a0087b2b5b4a658794eb4e0a35e18115baf5f
FPC-effortless <eweeewe917@gmail.com>
2026-08-26 01:51:47 +0100

    feat: export interactive CompanyWorld runtime

--- src/investigation_world/companyworld/__init__.py
+++ src/investigation_world/companyworld/__init__.py
@@ -15,6 +15,28 @@ from investigation_world.companyworld.distribution import (
     compile_expanded_episodes,
     compile_task_distribution,
 )
+from investigation_world.companyworld.interactive_distribution import (
+    INTERACTIVE_DISTRIBUTION_VERSION,
+    InteractiveCompanyWorldConfig,
+    compile_interactive_distribution,
+    compile_interactive_episode,
+)
+from investigation_world.companyworld.interactive_models import (
+    ActionEffectTemplate,
+    ActionExecution,
+    ActionPolicy,
+    InteractiveCompanyWorldEpisode,
+    InteractiveCompanyWorldOracle,
+    InteractiveCompanyWorldTask,
+    InteractiveCompanyWorldVerificationResult,
+    InteractiveOutcomeCondition,
+    OperationalAction,
+    OperationalActionType,
+    StateValue,
+)
+from investigation_world.companyworld.interactive_reference import solve_interactive_public
+from investigation_world.companyworld.interactive_runtime import InteractiveCompanyWorldRuntime
+from investigation_world.companyworld.interactive_verifier import verify_interactive_companyworld
 from investigation_world.companyworld.models import (
     CompanySystem,
     CompanyWorldEpisode,
@@ -33,7 +55,11 @@ from investigation_world.companyworld.runtime import (
 from investigation_world.companyworld.verifier import verify_companyworld
 
 __all__ = [
+    "INTERACTIVE_DISTRIBUTION_VERSION",
     "SYSTEM_TOOL_COSTS",
+    "ActionEffectTemplate",
+    "ActionExecution",
+    "ActionPolicy",
     "CompanySystem",
     "CompanyWorldAdapter",
     "CompanyWorldEpisode",
@@ -45,17 +71,31 @@ __all__ = [
     "CompanyWorldTaskDistributionConfig",
     "CompanyWorldValidationReport",
     "CompanyWorldVerificationResult",
+    "InteractiveCompanyWorldConfig",
+    "InteractiveCompanyWorldEpisode",
+    "InteractiveCompanyWorldOracle",
+    "InteractiveCompanyWorldRuntime",
+    "InteractiveCompanyWorldTask",
+    "InteractiveCompanyWorldVerificationResult",
+    "InteractiveOutcomeCondition",
+    "OperationalAction",
+    "OperationalActionType",
     "OperationalFactTarget",
+    "StateValue",
     "compile_companyworld",
     "compile_companyworld_distribution",
     "compile_expanded_episodes",
+    "compile_interactive_distribution",
+    "compile_interactive_episode",
     "compile_task_distribution",
     "oracle_bundle_payload",
     "public_bundle_payload",
     "public_distribution_payload",
+    "solve_interactive_public",
     "split_episode_ids",
     "stratified_split_episode_ids",
     "verify_companyworld",
+    "verify_interactive_companyworld",
     "write_companyworld_bundle",
     "write_companyworld_distribution_bundle",
 ]
