fd18912c1c007051ef31f6388b8d3aaa9b4263e7
FPC-effortless <eweeewe917@gmail.com>
2026-08-26 18:09:39 +0100

    Expose unified Veritas product surface

--- src/investigation_world/__init__.py
+++ src/investigation_world/__init__.py
@@ -1,4 +1,15 @@
 from .core.models import *
+from .operational import WorldDomain
+from .veritas import Veritas, VeritasProductInfo
 from .world.generator import WorldFactory, WorldGenerationConfig, validate_world
 
-__version__ = "0.4.0"
+__version__ = "0.6.0"
+
+__all__ = [
+    "Veritas",
+    "VeritasProductInfo",
+    "WorldDomain",
+    "WorldFactory",
+    "WorldGenerationConfig",
+    "validate_world",
+]
