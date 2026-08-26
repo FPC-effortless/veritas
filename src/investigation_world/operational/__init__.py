9a5d98268c4b3a1109d2135791c6da377b2357cb
FPC-effortless <eweeewe917@gmail.com>
2026-08-26 18:09:12 +0100

    Expose operational world API

---
+++ src/investigation_world/operational/__init__.py
@@ -0,0 +1,45 @@
+from investigation_world.operational.catalog import (
+    build_devops_incident_world,
+    build_enterprise_operations_world,
+    build_financial_spreadsheet_world,
+    build_gis_operations_world,
+    build_investigation_osint_world,
+    build_operational_suite,
+    build_operational_world,
+    operational_suite_manifest,
+)
+from investigation_world.operational.models import (
+    ActionKind,
+    EpisodeSubmission,
+    OperationalEpisode,
+    OperationalRecord,
+    OperationalSuiteManifest,
+    TaskContract,
+    VerificationBreakdown,
+    VerificationDimension,
+    WorldDomain,
+)
+from investigation_world.operational.runtime import OperationalRuntime
+from investigation_world.operational.verifier import verify_operational_episode
+
+__all__ = [
+    "ActionKind",
+    "EpisodeSubmission",
+    "OperationalEpisode",
+    "OperationalRecord",
+    "OperationalRuntime",
+    "OperationalSuiteManifest",
+    "TaskContract",
+    "VerificationBreakdown",
+    "VerificationDimension",
+    "WorldDomain",
+    "build_devops_incident_world",
+    "build_enterprise_operations_world",
+    "build_financial_spreadsheet_world",
+    "build_gis_operations_world",
+    "build_investigation_osint_world",
+    "build_operational_suite",
+    "build_operational_world",
+    "operational_suite_manifest",
+    "verify_operational_episode",
+]
