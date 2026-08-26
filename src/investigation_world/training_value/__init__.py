72bd14e66584ec1c4e1cf34b3cdd6833c7ea22eb
FPC-effortless <eweeewe917@gmail.com>
2026-08-26 07:15:39 +0100

    feat: export foundry transfer-training protocol

--- src/investigation_world/training_value/__init__.py
+++ src/investigation_world/training_value/__init__.py
@@ -3,9 +3,19 @@ from investigation_world.training_value.diagnostic_sft import (
     build_heldout_diagnostic_episodes,
     score_diagnostic_generator,
 )
+from investigation_world.training_value.foundry_transfer import (
+    build_training_rows,
+    build_transfer_suite,
+    score_generator,
+    select_frontier_examples,
+)
 
 __all__ = [
     "build_diagnostic_examples",
     "build_heldout_diagnostic_episodes",
+    "build_training_rows",
+    "build_transfer_suite",
     "score_diagnostic_generator",
+    "score_generator",
+    "select_frontier_examples",
 ]
