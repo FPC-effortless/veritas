aaf1f655be51a3de1644e5af972a4988cd9c144a
FPC-effortless <eweeewe917@gmail.com>
2026-08-26 21:52:19 +0100

    feat: expose ProjectWorld v2 qualification API

---
+++ src/investigation_world/projectworld/v2_qualification.py
@@ -0,0 +1,9 @@
+from investigation_world.qualification.projectworld import (
+    build_projectworld_v2_qualification_candidate,
+    execute_projectworld_v2_policy_suite,
+)
+
+__all__ = [
+    "build_projectworld_v2_qualification_candidate",
+    "execute_projectworld_v2_policy_suite",
+]
