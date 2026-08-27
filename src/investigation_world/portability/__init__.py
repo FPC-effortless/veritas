c3f990a75689e6fa2a5330afbfc5baca8cc767ab
Woyengi <eweeewe917@gmail.com>
2026-08-27 09:08:44 +0100

    Expose complete portability API

--- src/investigation_world/portability/__init__.py
+++ src/investigation_world/portability/__init__.py
@@ -1,3 +1,4 @@
+from investigation_world.portability.hud import build_hud_sre_package
 from investigation_world.portability.identity import portable_run_id, portable_task_id, state_digest
 from investigation_world.portability.models import (
     PortableArtifactReference,
@@ -12,7 +13,22 @@ from investigation_world.portability.models import (
     PortableVerifierContract,
     PortableVisibility,
 )
+from investigation_world.portability.package import (
+    PortablePackageBuildResult,
+    PortablePackageFile,
+    write_portable_package,
+)
+from investigation_world.portability.prime import build_prime_sre_package
+from investigation_world.portability.runtime import (
+    PortableEpisodeStart,
+    PortableGradeResult,
+    SREPortableRuntime,
+)
 from investigation_world.portability.sre import build_sre_portable_manifest
+from investigation_world.portability.sre_private import (
+    SREPrivatePortableTask,
+    build_sre_private_portable_tasks,
+)
 from investigation_world.portability.validation import (
     PortabilityValidationIssue,
     require_no_forbidden_tokens,
@@ -24,7 +40,11 @@ __all__ = [
     "PortableArtifactReference",
     "PortableCapability",
     "PortableEnvironmentManifest",
+    "PortableEpisodeStart",
+    "PortableGradeResult",
     "PortableMeteringContract",
+    "PortablePackageBuildResult",
+    "PortablePackageFile",
     "PortableReleaseIdentity",
     "PortableResetContract",
     "PortableSplit",
@@ -33,11 +53,17 @@ __all__ = [
     "PortableVerifierContract",
     "PortableVisibility",
     "PortabilityValidationIssue",
+    "SREPortableRuntime",
+    "SREPrivatePortableTask",
+    "build_hud_sre_package",
+    "build_prime_sre_package",
     "build_sre_portable_manifest",
+    "build_sre_private_portable_tasks",
     "portable_run_id",
     "portable_task_id",
     "require_no_forbidden_tokens",
     "require_portable_manifest",
     "state_digest",
     "validate_portable_manifest",
+    "write_portable_package",
 ]
