13e593d481fa1d11b57944fcbfe11a941bfa5553
Woyengi <eweeewe917@gmail.com>
2026-08-28 02:19:46 +0100

    Expose portable operational interfaces at package root

--- src/investigation_world/__init__.py
+++ src/investigation_world/__init__.py
@@ -1,6 +1,14 @@
 from .core.models import *
+from .mcp_compiler import MCP_PROTOCOL_VERSION, compile_mcp_surface
 from .operational import WorldDomain
+from .portable_contract import (
+    CONTRACT_SCHEMA_VERSION,
+    PortableOperationalContract,
+    PortablePublicContract,
+    compile_operational_episode,
+)
+from .portable_runtime import PortableOperationalRuntime, PortableRuntimeProtocol
 from .veritas import Veritas, VeritasCapability, VeritasCompany, VeritasProductInfo
 from .world.generator import WorldFactory, WorldGenerationConfig, validate_world
 
-__version__ = "0.8.0"
+__version__ = "0.11.0"
