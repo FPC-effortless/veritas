fc95c71e1f7bbbebcea7111b1e8d8abfe4468c63
Woyengi <eweeewe917@gmail.com>
2026-08-28 12:35:17 +0100

    Export environment attestation primitives

---
+++ src/investigation_world/attestations/__init__.py
@@ -0,0 +1,21 @@
+from investigation_world.attestations.models import (
+    ATTESTATION_SCHEMA_VERSION,
+    SIGNATURE_SCHEMA_VERSION,
+    ArtifactIdentity,
+    AttestationSignature,
+    ContentIdentity,
+    EnvironmentAttestation,
+    QualificationBinding,
+    serialize_attestation,
+)
+
+__all__ = [
+    "ATTESTATION_SCHEMA_VERSION",
+    "SIGNATURE_SCHEMA_VERSION",
+    "ArtifactIdentity",
+    "AttestationSignature",
+    "ContentIdentity",
+    "EnvironmentAttestation",
+    "QualificationBinding",
+    "serialize_attestation",
+]
